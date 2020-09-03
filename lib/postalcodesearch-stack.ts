c565dad8ef63487ce9267b8d1c907fc7273fd271
fourside <fourside@gmail.com>
2020-09-03 11:08:47 +0900

    lambda and apigateway construct

--- lib/postalcodesearch-stack.ts
+++ lib/postalcodesearch-stack.ts
@@ -1,9 +1,21 @@
-import * as cdk from "@aws-cdk/core";
+import { Stack, StackProps, Construct } from "@aws-cdk/core";
+import { AssetCode, Runtime, Function } from "@aws-cdk/aws-lambda";
+import { RetentionDays } from "@aws-cdk/aws-logs";
+import { LambdaRestApi } from "@aws-cdk/aws-apigateway";
 
-export class PostalcodesearchStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class PostalcodesearchStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const backend = new Function(this, "postalcodesearch", {
+      code: AssetCode.fromAsset(""),
+      handler: "index.handler",
+      runtime: Runtime.NODEJS_12_X,
+      logRetention: RetentionDays.SIX_MONTHS,
+    });
+
+    new LambdaRestApi(this, "postalcodesearchApi", {
+      handler: backend,
+    });
   }
 }
