09fc45a227fc6b1c56f020eb3ac86ba499ee2f1a
fourside <fourside@gmail.com>
2020-09-03 10:13:24 +0900

    prettier

--- bin/postalcodesearch.ts
+++ bin/postalcodesearch.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { PostalcodesearchStack } from '../lib/postalcodesearch-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { PostalcodesearchStack } from "../lib/postalcodesearch-stack";
 
 const app = new cdk.App();
-new PostalcodesearchStack(app, 'PostalcodesearchStack');
+new PostalcodesearchStack(app, "PostalcodesearchStack");
