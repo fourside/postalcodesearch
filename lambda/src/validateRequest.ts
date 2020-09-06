2e3e9578deaf26f9faeb079030961492735e46db
fourside <fourside@gmail.com>
2020-09-06 17:17:56 +0900

    wrong validation

--- lambda/src/validateRequest.ts
+++ lambda/src/validateRequest.ts
@@ -4,7 +4,7 @@ export function validateRequest(zipcode: string): boolean {
   if (!zipcode) {
     throw new ValidationException("zipcode required");
   }
-  if (zipcode.search(/^\d{7}$/) > -1) {
+  if (zipcode.search(/^\d{7}$/) === -1) {
     throw new ValidationException("zipcode 7 digits");
   }
   return true;
